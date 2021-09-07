cbec9031b9de6261013d30d572856ed7230623ef
Barney Rubble <barneyrubble.70777@gmail.com>
2021-09-07 13:25:59 -0500

    updating to java 16

--- src/main/java/com/bedrock/rsocketclient/health/controller/HealthController.java
+++ src/main/java/com/bedrock/rsocketclient/health/controller/HealthController.java
@@ -2,7 +2,6 @@ package com.bedrock.rsocketclient.health.controller;
 
 import com.bedrock.rsocketclient.health.dto.ClientHealthState;
 import java.time.Duration;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
@@ -13,9 +12,9 @@ public class HealthController {
 
   @MessageMapping("health")
   Flux<ClientHealthState> healthy() {
-    var stream = Stream.generate(() -> new ClientHealthState(Math.random() > 0.2)); //forces the client to be unhealthy at some point
     return Flux
-        .fromStream(stream)
+        //forces the client to be unhealthy at some point
+        .fromStream(Stream.generate(() -> new ClientHealthState(Math.random() > 0.2)))
         .delayElements(Duration.ofSeconds(1));
   }
 
